20490778a3733cb2f36346f6edba663669c65965
natansevero <natanseverolp@gmail.com>
2018-05-12 14:37:40 -0300

    monitorando erros, configurando debug e separando servidor da aplicação

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: 'node store api',
+        version: '0.0.2'
+    })
+})
+
+app.use('/', route);
+
+module.exports = app;
